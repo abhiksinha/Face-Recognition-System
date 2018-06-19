5e49cb5465ff644a9731aecf1654a78f9b4b9f5a
abhiksinha <40346971+abhiksinha@users.noreply.github.com>
2018-06-20 00:10:22 +0530

    Update and rename SystemTrain to SystemTrain.py

--- FaceRecognition/SystemTrain.py
+++ FaceRecognition/SystemTrain.py
@@ -7,31 +7,29 @@ recognizer = cv2.createLBPHFaceRecognizer()
 cascadePath = "Classifiers/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
 path = 'Samplefaces'
-print "adding to the training list"
+print "adding you to system"
 def get_images_and_labels(path):
+# Reading Images and Labels id
      image_paths = [os.path.join(path, f) for f in os.listdir(path)]
-     # images will contains face images
      images = []
-     # labels will contains the label that is assigned to the image
      labels = []
      for image_path in image_paths:
-         # Read the image and convert to grayscale
+     # converting RGB to Gray
          image_pil = Image.open(image_path).convert('L')
-         # Convert the image format into numpy array
+      # gray to numpy array for direct Matrix Operation
          image = np.array(image_pil, 'uint8')
-         # Get the label of the image
-         nbr = int(os.path.split(image_path)[1].split(".")[0].replace("face-", ""))
-         #nbr=int(''.join(str(ord(c)) for c in nbr))
+          # Seperating Image ids
+         lab = int(os.path.split(image_path)[1].split(".")[0].replace("face-", ""))
+         
          
          # Detect the face in the image
          faces = faceCascade.detectMultiScale(image)
          # If face is detected, append the face to images and the label to labels
          for (x, y, w, h) in faces:
              images.append(image[y: y + h, x: x + w])
-             labels.append(nbr)
+             labels.append(lab)
              cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
              cv2.waitKey(10)
-     # return the images list and labels list
      return images, labels
 
 
