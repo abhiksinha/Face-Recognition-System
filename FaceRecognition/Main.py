f043b635aa9e3386b180ccaf57fd471fdce45ce3
abhiksinha <40346971+abhiksinha@users.noreply.github.com>
2021-03-14 03:55:09 +0530

    Update Main.py

--- FaceRecognition/Main.py
+++ FaceRecognition/Main.py
@@ -1,45 +1,43 @@
-import cv2,os
-import numpy as np
-from PIL import Image 
-import pickle
-import time
+import cv2
 
-recognizer = cv2.createLBPHFaceRecognizer()
-recognizer.load('trainer/trainer.yml')
-cascadePath = "Classifiers/face.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+###This is the main code implemented to Recognize Real Time faces.
 
-pt=0
-pic = cv2.VideoCapture(0)
-font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1) 
-while pt<50:
-    ret, im =pic.read()
-    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    faces=faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
-    for(x,y,w,h) in faces:
-        id_predict, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-        cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
+##Initial Setup and Variables
+recognizer=cv2.face.LBPHFaceRecognizer_create() ### LBPH Face Recognizer 
+recognizer.read('trainer/trainer.yml')   ####Loading The Pre-trained dataset (Use SampleGen.py and Train.py to train your image)
+face_class=cv2.CascadeClassifier('Classifier\haarcascade_frontalface_default.xml')
+name=''
+cam=cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SIMPLEX 
+
+
+### Main Program
+
+while True:
+    ret,frame=cam.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_class.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+    
+    for x,y,w,h in faces:
+        identification,confidence= recognizer.predict(gray[y:y+h,x:x+w]) ### Lower Confidence means better result
+        
+        cv2.rectangle(frame,(x-20,y-20),(x+w+20,y+h+20),(255,255,255),2) ## Drawing a rectangle to the detected face 
         print(confidence)
         
-        if (confidence<55):
-                if(id_predict==1):
-                     Detected='Abhishek'
-                elif(id_predict==2):
-                    Detected='Subhash'
-                elif(id_predict==3):
-                     Detected='Rakesh'
-                elif(id_predict==4):
-                     Detected='Vijju'
-                elif(id_predict==5):
-                     Detected='Abhinav'
-                elif(id_predict==6):
-                     Detected='Chotu'     
+        if confidence<50:      ## Checking confidence level. If else statement adjust according to the users(use dictionary for ease)
+            if identification==1:
+                name='user1'
+            elif identification==2:
+                name='user2'
         else:
-            Detected='not found'
-        cv2.cv.PutText(cv2.cv.fromarray(im),str(Detected)+"--"+str(confidence), (x,y+h),font, 255) 
-        cv2.imshow('im',im)
-        cv2.waitKey(10)
-        pt=pt+1
-        
-pic.release()        
+            name='Undetected'
+            
+        frame = cv2.putText(frame,name,(x,y+h),font,1,(0,255,0)) ###Writing the name of the recognized person
+        cv2.imshow('Real Time Face Recognition',frame)
+    
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cam.release()
 cv2.destroyAllWindows()
+                
