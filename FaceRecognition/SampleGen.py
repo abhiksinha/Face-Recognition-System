2f5efb74a08892fdfadc3ab5c7457ca4f61fb86d
abhiksinha <40346971+abhiksinha@users.noreply.github.com>
2021-03-13 23:31:21 +0530

    Update SampleGen.py

--- FaceRecognition/SampleGen.py
+++ FaceRecognition/SampleGen.py
@@ -1,24 +1,47 @@
-# Note this Example is tested only in Python 2.7
-# This is a real time system but processing time may depends upon your processor
-# This Code Generates Sample Faces 
+### This Code is used to Generate Sample Data Set which is used to Train our system.
+
 import cv2
-From ids import id
-pic = cv2.VideoCapture(0) 
-det=cv2.CascadeClassifier('Classifiers/face.xml')
-i=0
-offset=50
-id=id+1
+
+face_class=cv2.CascadeClassifier('Classifier\haarcascade_frontalface_default.xml') ## Importing trained dataset of front face
+
+##Global Variables
+offset=20 ## adjust the offset to get full face
+samples=0
+identification=2 ### set different values for different persons
+
+### This Function Detects and Crop the face from the image
+
+def detect_face_and_crop(img):
+    
+        global identification,samples
+        
+        face_img=img.copy()
+        gray=cv2.cvtColor(face_img,cv2.COLOR_BGR2RGB) ##converting to grayscale
+        faces=face_class.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,flags=cv2.CASCADE_SCALE_IMAGE) ##Detect the face,parameters like minNeighbers, scaleFactor should be adjusted 
+
+        for x,y,w,h in faces:
+            crop=gray[y-offset:y+h+offset,x-offset:x+w+offset] ## Cropping the Face from the gray scale image. Adjust the variable offset if full face is not cropped  
+            samples=samples+1
+            cv2.imwrite("SampleFace/face-"+str(identification) +'.'+ str(samples) + ".jpg",crop) ## Saving the cropped image
+            
+            cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),2) ##drawing rectangle to the color image(frame)
+        return face_img
+
+
+cam=cv2.VideoCapture(0)
+
+### Smile please :p
 while True:
-    ret, im =pic.read()
-    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    cropFace=det.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
-    for(x,y,w,h) in cropFaces:
-        i=i+1
-        crop=gray[y-offset:y+h+offset,x-offset:x+w+offset]
-        cv2.imwrite("SampleFaces/face-"+id +'.'+ str(i) + ".jpg",crop )
-        cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
-        cv2.waitKey(100)
-    if i>50:
-        pic.release()
-        cv2.destroyAllWindows()
-        break
+        ret,frame=cam.read()
+        frame=detect_face_and_crop(frame)
+        cv2.imshow('face',frame)
+        
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+
+        if samples>50:
+            break
+        
+cam.release()    
+cv2.destroyAllWindows()    
+
